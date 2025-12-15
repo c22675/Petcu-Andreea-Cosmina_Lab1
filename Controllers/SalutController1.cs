d07b3efe2cb59698ae439839ad04a3a1d78132fc
Cosmina Andreea Petcu <cosminapetcu16@gmail.com>
2025-12-15 22:46:41 +0200

    Primul controller creat

---
+++ Controllers/SalutController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Petcu_Andreea_Cosmina_Lab1.Controllers
+{
+    public class SalutController1 : Controller
+    {
+        private string mesaj_welcome = "bine v-am gasit!";
+        private string mesaj_salut = "salut!";
+
+        public string Index()
+        {
+            return mesaj_welcome;
+        }
+
+        public string Salut()
+        {
+            return mesaj_salut;
+        }
+
+        public string AfiseazaDate(string text, int numar)
+        {
+            return $"Ai introdus textul: \"{text}\" și numărul: {numar}.";
+        }
+    }
+}
+
+
